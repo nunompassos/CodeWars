8c2019775a358945f5a118c1ba1e52020f0d8c9d
Nuno Passos <nunompassos@gmail.com>
2023-12-14 08:24:23 +0000

    7 Kyu - Overflowing with joy - improved with a method call

--- Java/src/main/java/com/github/nunompassos/kyu/seventh/OverflowingWithJoy.java
+++ Java/src/main/java/com/github/nunompassos/kyu/seventh/OverflowingWithJoy.java
@@ -7,10 +7,6 @@ package com.github.nunompassos.kyu.seventh;
  */
 public class OverflowingWithJoy {
     public static int multiply(int a, int b) {
-        long t = (long) a * b;
-        if (t > Integer.MAX_VALUE || t < Integer.MIN_VALUE) {
-            throw new ArithmeticException("overflow");
-        }
-        return (int) t;
+        return Math.multiplyExact(a, b);
     }
 }
